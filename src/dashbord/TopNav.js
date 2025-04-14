e195f28804494c124cc5aba6fd7c25540a765d3b
farhangin-elham <elhamfarhangian6@gmail.com>
2025-04-14 08:19:38 +0330

    aa11

--- src/dashbord/TopNav.js
+++ src/dashbord/TopNav.js
@@ -60,7 +60,7 @@ const Buy = styled('div')(({ theme }) => ({
     width: '150px',
   },
 }));
-function TopNav(props) {
+function TopNav() {
 
 
 
@@ -139,8 +139,8 @@ setFilteredUsers(filteredItems);
 }
   return (
     <>
-     <HideOnScroll {...props}>
-      {/*
+   
+
     <AppBar position="fixed" className='nav' sx={{mt:0 ,mb:0,bgcolor:'black',px:"0.5em",color:'white'}}>
     <Box sx={{ flexGrow: 1,py:0, display:'flex',justifyContent:'space-between',marginLeft:{md:0},marginRight:{md:0} }}>
         <Grid  sx={{mt:"0.2em", display:'flex', boxShadow:'10px 10px 50px #674e06' }}>
@@ -183,150 +183,7 @@ setFilteredUsers(filteredItems);
 
 </Box>
     </AppBar>
-    */}
     
-<nav dir='rtl' className=' md:sticky top-0 flex justify-between w-[100%] bg-black h-[60px]  drop-shadow-lg pt-1  pl-1  mx-auto '>
-<div dir='ltr' className="w-2/3 flex flex-row justify-end mt-0">
-
-      
-	<form className="flex items-center mx-4">   
-        <label for="simple-search" className="sr-only">Search</label>
-        <div dir='rtl' className="relative w-full ">
-            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                <svg className="w-10 h-5 text-yellow-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-            </div>
-            <input  onChange={() => setOpen(true)} type="text" id="simple-search" className=" bg-black border border-yellow-600
-             text-gray-700 text-sm rounded-lg
-             hover:border-2 hover:border-yellow-600 block w-full pl-10 p-2.5" placeholder="جستجو ..." required />
-        </div>
-      
-    </form>
-    <div dir='rtl' className="flex justify-center w-[220px]">
-        <img className='imgsohil' width={50} height={30} alt='' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi8sUcl-xSeJ2aDKi3dB9ylFBIxBLCo-JXLg&s'/>
-             <NavLink to={"/"} style={{textDecoration: "none"}}>  <p className="flex italic font-mono text-yellow-600 text-2xl space-x-2 mt-2"
-   >
-     S O H I L
-   </p>
-   </NavLink>
-   </div>  
-    <Modal
-       
-       aria-labelledby="modal-title"
-       aria-describedby="modal-desc"
-       open={open}
-       onClose={() => setOpen(false)}
-       sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
-      >
-<ModalDialog sx={{bgcolor:"#ececec"}}
-layout="fullscreen"
-size="lg"
->
-<ModalClose  />
-<div className='flex justify-between self-center bg-neutral-200 w-[80%] px-1 my-6'
- >
-<IconButton type="button" sx={{ p: '10px' }}  >
-<svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-        </svg>
-  </IconButton> 
-  <InputBase
-    sx={{direction:"rtl", ml: 1, flex: 1 }}
-    type="text"
-    placeholder="جستجوی محصولات نهال شاپ"
-    inputProps={{ 'aria-label': 'جستجوی محصولات نهال شاپ' }}
-    value={searchItem}
-    onChange={
-      handleInputChange
-    }
-  />
-</div>
-
-
-<div className='overflow-y-scroll '>
-{filteredUsers.map((item) => (
-<NavLink to={'/buy'}  className={"linksearch"}>  
-
-<div  onClick={()=>{
-  setCartItems([item]);
-  setOpen(false);}}
- dir='rtl' className='hidden md:flex md:flex-wrap md:flex-row md:justify-around md:self-center w-[92%] h-[260px] bg-white my-2  border-2 border-gray-200 mx-auto  '>
-
- <div 
- className='shh relative w-[250px] h-[250px] rounded-sm 
- bg-cover bg-center bg-no-repeat
-   before:absolute before:w-full before:h-[60px] before:bottom-[-12px] before:rounded-3xl before:bg-transparent
- ' style={{
-  backgroundImage: `url(${item.img})`,borderRadius:"23% 77% 11% 89% / 66% 42% 58% 34% "
-}} >  
-  </div>
-
-<div className=' inline-flex justify-start self-center items-center py-1 w-[250px] '>
-<div className='w-full py-1'> <p className='text-xl text-center flex-nowrap '>{item.title1}</p></div>
-
-</div>
-<div className='w-[150px]   inline-flex justify-start self-center items-center'  > 
-<p className='text-[20px] text-gray-500  text-right font-bold'   >
-{addCommas(convertToPersian((convertToEnglish(item.pricenum1)-(convertToEnglish(item.pricenum1)*item.off / 100))+''))} تومان 
-</p>
-</div>
-</div> 
-<div  onClick={()=>{
-setCartItems([item]);
-setOpen(false);}}
-dir='rtl' className='flex flex-row justify-around self-center md:hidden w-full h-[150px] bg-white my-2  border-2 border-gray-200 mx-auto  '>
-
-<div 
- className='px-2 shh relative w-1/3 h-[120px] rounded-sm 
- bg-cover bg-center bg-no-repeat
-   before:absolute before:w-full before:h-[60px] before:bottom-[-12px] before:rounded-3xl before:bg-transparent
- ' style={{
-  backgroundImage: `url(${item.img})`,borderRadius:"23% 77% 11% 89% / 66% 42% 58% 34% "
-}} >  
-  </div>
-
-<div className=' inline-flex justify-start self-center items-center py-1  w-1/3 px-2 '>
-<div className='w-full py-1'> <p className='text-sm text-center flex-nowrap '>{item.title1}</p></div>
-
-</div>
-<div className=' w-1/3   inline-flex justify-start self-center items-center px-2'  > 
-<p className='text-[16px] text-gray-500  text-right font-bold'   >
-{addCommas(convertToPersian((convertToEnglish(item.pricenum1)-(convertToEnglish(item.pricenum1)*item.off / 100))+''))} تومان 
-</p>
-</div>
-</div> 
-
-</NavLink>
-))}
-
-</div>
-
-
-
-
-
-
-
-
-</ModalDialog>
-</Modal>
-
-
-</div>
-<div className=" flex  justify-between pt-2 mt-2 pl-6 py-0 h-8 ">
-<NavLink to={"/signup"}  style={{display:"flex",textAlign:"center", cursor:"pointer", alignItems:"center",fontWeight:600}}
->
-<RiRoadMapLine className='font-black w-7 h-7 text-yellow-600 hover:text-white mx-6 ' />
-</NavLink>
-<Badge color='warning' badgeContent={cartItemsBuy.length} sx={{color:"white"}}>
-<NavLink to={"/endbuy"}  style={{display:"flex",textAlign:"center", cursor:"pointer", alignItems:"center",fontWeight:600}}
->
-<RiShoppingCartLine className='font-black w-7 h-7  text-yellow-600 hover:text-white ' />
-</NavLink>
-</Badge>
-</div>
-</nav>
-    </HideOnScroll>
     <AppBar position='sticky' className='nav' sx={{display:{xs:'none',lg:'flex'},px:"0.5em",
       mt:"5.6em" ,mb:0,bgcolor:'black',py:0,color:'white',}}>
           <Box sx={{ flexGrow: 1, display:{ xs:'none',lg: 'flex'}, justifyContent:'space-between',marginLeft:{md:0},marginRight:{md:0} }}>
